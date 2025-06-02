1e7f62e42aa705060538db8f35aa53f5c03a990d
tanay1845 <tanay23186@gmail.com>
2025-06-02 21:18:47 +0530

    bug fix in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+
 const registerUser = asyncHandler( async(req,res)=>{
     // get user details
     // check the user already exists
@@ -16,36 +17,45 @@ const registerUser = asyncHandler( async(req,res)=>{
     // return response
 
     const {username , email ,fullName ,password} = req.body
-    console.log("email :",email,"Password :",password,"Full Name :",fullName,"Username",username)
+    // console.log("email :",email,"Password :",password,"Full Name :",fullName,"Username",username)
 
     if([fullName,email,username,password].some((field) => (field?.trim() === ""))){
-        throw new ApiError(400,"All fields are required")
+        throw new ApiError(400,"All fields are required");
     }
 
-    const existingUser = User.findOne({
+    const existingUser = await User.findOne({
         $or: [{ email } , { username }]
     })
 
     if(existingUser){
-        ApiError(409,"User already exists")
+        throw new ApiError(409,"User already exists");
+    }
+    
+    const avatarLocalPath = req.files.avatar[0].path;
+    // const coverImageLocalPath =  req.files?.coverImage[0].path;
+
+    // console.log(avatarLocalPath)
+
+    let coverImageLocalPath;
+    
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
     }
 
-    const avatarLocalPath =  req.files?.avatar[0].path;
-    const coverImageLocalPath = req.files?.coverImage[0].path;
-
     if(!avatarLocalPath){
-        ApiError(400,"Avtar field is required")
+        throw new ApiError(400,"Avtar field is required")
     }
     
     const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    // console.log(avatar,coverImage)
 
     if(!avatar){
-        ApiError(400,"Avatar image is required.")
+        throw new ApiError(400,"Avatar image is required.")
     }
 
     const user = await User.create({
-        avatar,
+        fullName,
         avatar : avatar.url,
         coverImage : coverImage?.url || "",
         email,
@@ -58,7 +68,7 @@ const registerUser = asyncHandler( async(req,res)=>{
     )
 
     if(!createdUser){
-        ApiError(500, "Something went wrong while registering the user")
+        throw new ApiError(500, "Something went wrong while registering the user")
     }
 
     return res.status(201).json(
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,27 +1,35 @@
 import { v2 as cloudinary } from 'cloudinary';
-import fs from "fs";
+import fs from "fs"
+import dotenv from "dotenv"
+
+dotenv.config({})
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_SECRET_KEY
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath)  return null
+        if (!localFilePath) return null
         // upload the file on cloudinary
 
-        const response = await cloudinary.uploader.upload(localFilePath,{
-            resource_type:'auto',
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto',
         });
         // file has been uploded successfully
-        console.log("File is uploded on clodinary",response.url);
+
+        // console.log("File is uploded on clodinary", response.url);
+
+        fs.unlinkSync(localFilePath)
         return response;
+        
     } catch (error) {
-        fs.unlink(localFilePath)
+        fs.unlinkSync(localFilePath)
+        console.log(error)
+        return null
         // removes the locally saved temporary file as the upload operation get failed
-        return null 
     }
 }
 
